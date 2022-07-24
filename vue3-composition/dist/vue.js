5263ac31997105cad0969abe553c0f4b09931a5f
aicherish <aicherish0320@163.com>
2022-07-24 09:30:10 +0800

    手写源码配置文件

---
+++ vue3-composition/dist/vue.js
@@ -0,0 +1,10 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+})((function () { 'use strict';
+
+	const msg = 'Hello';
+	console.log('Hello >>> ', msg);
+
+}));
+//# sourceMappingURL=vue.js.map
