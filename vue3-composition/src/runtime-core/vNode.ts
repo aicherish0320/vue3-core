6454e6b53dfd1b0d8c55d82bedfd6e72738d159f
aicherish <aicherish0320@163.com>
2022-07-26 08:02:15 +0800

    vue3 的初始化操作上

--- vue3-composition/src/runtime-core/vNode.ts
+++ vue3-composition/src/runtime-core/vNode.ts
@@ -1,11 +1,26 @@
+import { isObject, isString, shapeFlags, isArray } from '../shared'
+import { ShapeFlags } from '@vue/shared'
+
 export function createVNode(type, props: any = {}, children = null) {
-  const vNode = {
+  const shapeFlag = isString(type)
+    ? shapeFlags.ELEMENT
+    : isObject(type)
+    ? shapeFlags.STATEFUL_COMPONENT
+    : 0
+
+  const vNode: any = {
     type,
     props,
     children,
     component: null, // 组件实例
     el: null,
     key: props.key,
-    shapeFlag: true
+    shapeFlag: true // 表示虚拟节点的类型：元素、组件
+  }
+
+  if (isArray(children)) {
+    vNode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN
+  } else {
+    vNode.shapeFlag |= ShapeFlags.TEXT_CHILDREN
   }
 }
--- vue3-composition/src/shared/index.ts
+++ vue3-composition/src/shared/index.ts
@@ -5,3 +5,6 @@ export const isInteger = (key) => parseInt(key, 10) + '' === key
 const hasOwnProperty = Object.prototype.hasOwnProperty
 export const hasOwn = (val, key) => hasOwnProperty.call(val, key)
 export const hasChanged = (value, oldValue) => value !== oldValue
+export const isString = (val) => typeof val === 'string'
+
+export * from './shapeFlags'
