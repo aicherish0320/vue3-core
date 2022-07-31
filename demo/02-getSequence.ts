38efbd41f58a9472f2bb5a1069abc5e345ebc1ed
aicherish <aicherish0320@163.com>
2022-07-31 21:26:36 +0800

    最长递增子序列

---
+++ demo/02-getSequence.ts
@@ -0,0 +1,47 @@
+function getSequence(arr: number[]) {
+  const p = arr.slice()
+  // 默认以0开头
+  const result = [0]
+  const len = arr.length
+  let i, j, u, v, c
+
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i]
+    if (arrI !== 0) {
+      j = result[result.length - 1]
+      if (arr[j] < arrI) {
+        p[i] = j
+        result.push(i)
+        continue
+      }
+      u = 0
+      v = result.length - 1
+      while (u < v) {
+        c = (u + v) >> 1
+        if (arr[result[c]] < arrI) {
+          u = c + 1
+        } else {
+          v = c
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1]
+        }
+        result[u] = i
+      }
+    }
+  }
+  u = result.length
+  v = result[u - 1]
+  while (u-- > 0) {
+    result[u] = v
+    v = p[v]
+  }
+
+  return result
+}
+
+getSequence([0, 1, 2, 3, 4, 5])
+
+export {}
